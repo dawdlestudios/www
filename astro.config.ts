90bbe9e106b57647c12bdad7dcac21738ccf99a4
Henry Gressmann <mail@henrygressmann.de>
2023-12-13 12:39:51 +0100

    redesign :)
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

---
+++ astro.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [sitemap()],
+    site: "https://dawdle.space",
+    compressHTML: true,
+    prefetch: true,
+});
+
+ 
