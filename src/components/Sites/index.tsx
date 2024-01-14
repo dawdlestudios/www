af2536169afb6a958c0e2dfaaf4000438444d2df
Henry Gressmann <mail@henrygressmann.de>
2024-01-14 18:17:25 +0100

    dynamic sites list
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

---
+++ src/components/Sites/index.tsx
@@ -0,0 +1,51 @@
+import styles from "./sites.module.css";
+import useSWR from "swr";
+import { fetchJson } from "../../utils/api";
+
+type Site = {
+	type: "user" | "site";
+	username: string;
+	hostname?: string;
+};
+
+export const SitesList = () => {
+	const { data, isLoading, error } = useSWR<Site[]>("/api/sites", fetchJson);
+
+	const users = data?.filter((site) => site.type === "user" && site.username !== "test");
+	const sites = data?.filter((site) => site.type === "site");
+
+	return (
+		<main className={styles.main}>
+			{isLoading && <p>Loading...</p>}
+			{error && <p>Error loading sites</p>}
+
+			{!isLoading && (
+				<>
+					<h1>Members</h1>
+					<ul>
+						{users?.map((user) => (
+							<li key={user.username}>
+								{">>"} <a href={`https://${user.username}.dawdle.space`}>~{user.username}</a>
+							</li>
+						))}
+					</ul>
+
+					<h1>Sites</h1>
+					<ul>
+						{sites?.map((site) => {
+							const actualHostname = site.hostname?.includes(".")
+								? site.hostname
+								: `${site.hostname}.dawdle.space`;
+							return (
+								<li key={site.hostname}>
+									{">>"} <a href={`https://${actualHostname}`}>{site.hostname}</a> by{" "}
+									<a href={`https://${site.username}.dawdle.space`}>~{site.username}</a>
+								</li>
+							);
+						})}
+					</ul>
+				</>
+			)}
+		</main>
+	);
+};
